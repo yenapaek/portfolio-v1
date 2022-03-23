ece0a79e97f60509ab753fd2cdfbe852c7f75499
Yena <yenapaek@gmail.com>
2022-03-23 16:11:50 -0400

    added basic layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,191 +1,196 @@
 import React from 'react';
+import { BsArrowUpCircle, BsArrowRight } from 'react-icons/bs';
 
 class Home extends React.Component {
     render(){
         return(
             <div className="homeCont">
                 <div className="headerBlock">
-                    <div className="homeMainText"><h2>Hi there!</h2></div>
-                    <div className="homeMainText"><h2>I'm <strong>Yena Paek</strong>, a design-minded software engineer.</h2></div>
+                    <div className="homeMainText">Hi there!</div>
+                    <div className="homeMainText">I'm <strong>Yena</strong>, a design-minded software engineer.</div>
                     <div className="btnClear">Get in touch</div>
                 </div>
-                <div className="colBlock">
-                    <div className="blockLabel">
-                        <h2>About</h2>
-                    </div>
-                    <div>
-                        <p>I'm currently an Engineer at Brave building things for the web.</p>
-                    </div>
-                </div>
-                <div className="colBlock">
-                    <div className="blockLabel"><h2>Skills</h2></div>
-                    <div className="skillsList">
-                        <div>
-                            <h3>Languages</h3>
-                            <ul>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h3>Frameworks</h3>
-                            <ul>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h3>Tools</h3>
-                            <ul>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h3>Design</h3>
-                            <ul>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                                <li>JavaScript (ES6)</li>
-                            </ul>
-                        </div>
+                <div className="contentBlock">
+                    <div className="blockLabel">About</div>
+                    <div className="blockContent">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     </div>
                 </div>
-                <div className="colBlock">
-                    <div className="blockLabel"><h2>Experience</h2></div>
-                    <div className="experienceList">
-                        <div className="experienceRow">
-                            <div>
-                                <h4>Software Developer</h4>
-                                <p>Raw Korean</p>
+                <div className="contentBlock">
+                    <div className="blockLabel">Skills</div>
+                    <div className="blockContent">
+                        <div className="skills">
+                            <div className="skill">
+                                <div className="skillTitle">Languages</div>
+                                <ul>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                </ul>
                             </div>
-                            <div>May 2018 - Present</div>
-                        </div>
-                        <div className="experienceRow">
-                            <div>
-                                <h4>Brave</h4>
-                                <p>Engineer</p>
+                            <div className="skill">
+                                <div className="skillTitle">Frameworks</div>
+                                <ul>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                </ul>
                             </div>
-                            <div>May 2018 - Present</div>
-                        </div>
-                        <div className="experienceRow">
-                            <div>
-                                <h4>Brave</h4>
-                                <p>Engineer</p>
+                            <div className="skill">
+                                <div className="skillTitle">Tools</div>
+                                <ul>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                </ul>
                             </div>
-                            <div>May 2018 - Present</div>
-                        </div>
-                        <div className="experienceRow">
-                            <div>
-                                <h4>Brave</h4>
-                                <p>Engineer</p>
+                            <div className="skill">
+                                <div className="skillTitle">Design</div>
+                                <ul>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                    <li>JavaScript (ES6)</li>
+                                </ul>
                             </div>
-                            <div>May 2018 - Present</div>
                         </div>
-                        <div className="experienceRow">
-                            <div>
-                                <h4>Brave</h4>
-                                <p>Engineer</p>
-                            </div>
-                            <div>May 2018 - Present</div>
-                        </div>
-                        <div className="experienceRow">
-                            <div>
-                                <h4>Brave</h4>
-                                <p>Engineer</p>
-                            </div>
-                            <div>May 2018 - Present</div>
-                        </div>
-                        <div>View My Resume</div>
                     </div>
                 </div>
-                <div className="colBlock">
-                    <div className="blockLabel">            
-                        <h2>Projects</h2>
+                <div className="contentBlock">
+                    <div className="blockLabel">Experience</div>
+                    <div className="blockContent">
+                        <div className="jobs">
+                            <div className="job">
+                                <div className="jobInfo">
+                                    <div className="jobTitle">Software Developer</div>
+                                    <div className="jobCompany"><a>Raw Korean</a></div>
+                                </div>
+                                <div className="jobDate">May 2018 - Present</div>
+                            </div>
+                            <div className="job">
+                                <div className="jobInfo">
+                                    <div className="jobTitle">Software Developer</div>
+                                    <div className="jobCompany"><a>Raw Korean</a></div>
+
+                                </div>
+                                <div className="jobDate">May 2018 - Present</div>
+                            </div>
+                            <div className="job">
+                                <div className="jobInfo">
+                                    <div className="jobTitle">Software Developer</div>
+                                    <div className="jobCompany"><a>Raw Korean</a></div>
+
+                                </div>
+                                <div className="jobDate">May 2018 - Present</div>
+                            </div>
+                            <div className="job">
+                                <div className="jobInfo">
+                                    <div className="jobTitle">Software Developer</div>
+                                    <div className="jobCompany"><a>Raw Korean</a></div>
+
+                                </div>
+                                <div className="jobDate">May 2018 - Present</div>
+                            </div>
+                            <div className="job">
+                                <div className="jobInfo">
+                                    <div className="jobTitle">Software Developer</div>
+                                    <div className="jobCompany"><a>Raw Korean</a></div>
+
+                                </div>
+                                <div className="jobDate">May 2018 - Present</div>
+                            </div>
+                            <div className="job">
+                                <div className="jobInfo">
+                                    <div className="jobTitle">Software Developer</div>
+                                    <div className="jobCompany"><a>Raw Korean</a></div>
+
+                                </div>
+                                <div className="jobDate">May 2018 - Present</div>
+                            </div>
+                        </div>    
                     </div>
-                    <div className="projectsList">
-                        <div className="projectRow">
-                            <div className="projectTitle">
-                                <h4>Project Title</h4>
-                                <p>React Native project created during a Hackathon.</p>
+                </div>
+                <div className="contentBlock">
+                    <div className="blockLabel">Projects</div>
+                    <div className="blockContent">
+                        <div className="projects">
+                            <div className="project">
+                                <div className="projectInfo">
+                                    <div className="projectTitle">Project Title <a href="" target="_blank"><BsArrowRight /></a></div>
+                                    <p>React Native project created during a Hackathon.</p>
+                                </div>
+                                <div className="projectTechList">
+                                    <div>JavaScript</div>
+                                    <div>React</div>
+                                    <div>React Router</div>
+                                    <div>Axios</div>
+                                    <div>YouTube API</div>
+                                </div>
                             </div>
-                            <div className="projectTechList">
-                                <div>JavaScript</div>
-                                <div>React</div>
-                                <div>React Router</div>
-                                <div>Axios</div>
-                                <div>YouTube API</div>
+                            <div className="project">
+                                <div className="projectInfo">
+                                   <div className="projectTitle">Project Title <a href="" target="_blank"><BsArrowRight /></a></div>
+                                    <p>React Native project created during a Hackathon.</p>
+                                </div>
+                                <div className="projectTechList">
+                                    <div>JavaScript</div>
+                                    <div>React</div>
+                                    <div>React Router</div>
+                                    <div>Axios</div>
+                                    <div>YouTube API</div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="projectRow">
-                            <div className="projectTitle">
-                                <h4>Project Title</h4>
-                                <p>React Native project created during a Hackathon.</p>
+                            <div className="project">
+                                <div className="projectInfo">
+                                   <div className="projectTitle">Project Title <a href="" target="_blank"><BsArrowRight /></a></div>
+                                    <p>React Native project created during a Hackathon.</p>
+                                </div>
+                                <div className="projectTechList">
+                                    <div>JavaScript</div>
+                                    <div>React</div>
+                                    <div>React Router</div>
+                                    <div>Axios</div>
+                                    <div>YouTube API</div>
+                                </div>
                             </div>
-                            <div className="projectTechList">
-                                <div>JavaScript</div>
-                                <div>React</div>
-                                <div>React Router</div>
-                                <div>Axios</div>
-                                <div>YouTube API</div>
+                            <div className="project">
+                                <div className="projectInfo">
+                                   <div className="projectTitle">Project Title <a href="" target="_blank"><BsArrowRight /></a></div>
+                                    <p>React Native project created during a Hackathon.</p>
+                                </div>
+                                <div className="projectTechList">
+                                    <div>JavaScript</div>
+                                    <div>React</div>
+                                    <div>React Router</div>
+                                    <div>Axios</div>
+                                    <div>YouTube API</div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="projectRow">
-                            <div className="projectTitle">
-                                <h4>Project Title</h4>
-                                <p>React Native project created during a Hackathon.</p>
-                            </div>
-                            <div className="projectTechList">
-                                <div>JavaScript</div>
-                                <div>React</div>
-                                <div>React Router</div>
-                                <div>Axios</div>
-                                <div>YouTube API</div>
-                            </div>
-                        </div>
-                        <div className="projectRow">
-                            <div className="projectTitle">
-                                <h4>Project Title</h4>
-                                <p>React Native project created during a Hackathon.</p>
-                            </div>
-                            <div className="projectTechList">
-                                <div>JavaScript</div>
-                                <div>React</div>
-                                <div>React Router</div>
-                                <div>Axios</div>
-                                <div>YouTube API</div>
-                            </div>
-                        </div>
+                        </div>    
                     </div>
                 </div>
                 <div className="footer">
-                    <div>
-                        <h4>Designed & Developed by
-Yena Paek</h4>
-                    </div>
+                    <div className="footerLabel">Designed & Built by Yena Paek</div>
                     <div className="footerLinks">
-                        <ul>
-                            <li>Email</li>
-                            <li>GitHub</li>
-                            <li>LinkedIn</li>
-                        </ul>
+                        <a>Email</a>
+                        <a>LinkedIn</a>
+                        <a>GitHub</a>
                     </div>
                 </div>
+                <div className="floating"><BsArrowUpCircle /></div>
             </div>
         );
     }
